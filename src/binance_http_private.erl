a4280bd8d457b2bfb8a45e3c4991720b48444d8d
Shvein Anton <t0hashvein@gmail.com>
2020-02-05 11:48:41 +0500

    Added timeout to recurse reasons

--- src/binance_http_private.erl
+++ src/binance_http_private.erl
@@ -296,6 +296,8 @@ handle_responce(Connection, Ref, {response, nofin, _Code, _Headers}) ->
     handle_body(gun:await_body(Connection, Ref));
 handle_responce(_Connection, _Ref, {error, {stream_error, closed}}) ->
     recurse;
+handle_responce(_Connection, _Ref, {error, timeout}) ->
+    recurse;
 handle_responce(_Connection, _Ref, {error, E}) ->
     {ok, #{<<"error">> => iolist_to_binary(io_lib:format("~p", [E]))}}.
 
